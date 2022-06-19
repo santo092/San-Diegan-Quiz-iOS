d49b4717a430418427829a6bbadaf9220e169bd1
Tsantos <trixiea007@gmail.com>
2022-06-19 01:21:38 -0700

    Initial Commit

---
+++ San Diegan Quiz iOS/San_Diegan_Quiz_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  San_Diegan_Quiz_iOSApp.swift
+//  San Diegan Quiz iOS
+//
+//  Created by Trixie Santos on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct San_Diegan_Quiz_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
